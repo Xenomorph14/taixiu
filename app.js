15d5d10ff78c4458eb67e3effcc8ca8e2a141380
thang <boypham1234567@gmail.com>
2020-08-11 20:45:21 +0700

    add localstorage

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ function rdDice() {
 //BET
 let result_totalTai = 0;
 let result_totalXiu = 0;
+money.innerText=localStorage.getItem("money");
 btnBet.addEventListener("click", () => {
   if (start <= 0) {
     showStatus("Hết giờ đặt cược");
@@ -39,6 +40,7 @@ btnBet.addEventListener("click", () => {
     money.innerText = Number(money.innerText) - Number(inputBetXiu.value);
     inputBetTai.value = 0;
     inputBetXiu.value = 0;
+    localStorage.setItem("money",money.innerText);
   }
 });
 btnClear.addEventListener("click", () => {
